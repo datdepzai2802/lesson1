5d6ca8276f40ad2855bb6ee128ce8331c5b376cf
ngotiendat <datmau20@gmail.com>
2022-05-09 10:41:22 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+var iconbar = document.getElementById("iconbar");
+var close = document.getElementById("close");
+var navbar = document.getElementById("navbar");
+
+function navOpen() {
+  navbar.style.width = "50%";
+  navbar.style.display = "block";
+}
+function navClose() {
+  navbar.style.width = 0;
+  navbar.style.display = "none";
+}
