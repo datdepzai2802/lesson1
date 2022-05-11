10bf4f9b5f2a003e9529f9b53bc4b9e9d548a986
ngotiendat <datmau20@gmail.com>
2022-05-11 14:58:57 +0700

    load2

--- main.js
+++ main.js
@@ -10,3 +10,30 @@ function navClose() {
   navbar.style.width = 0;
   navbar.style.display = "none";
 }
+
+const arrImg = [
+  "img/slide-tieng-han-pc.jpg",
+  "img/slide-tieng-trung-pc.jpg",
+  "img/slide-tieng-anh-pc.jpg",
+];
+let slides = document.querySelector(".slideImg");
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
